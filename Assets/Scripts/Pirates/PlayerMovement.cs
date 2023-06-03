0a1e7853aa42bb5ff0c79fe238f5dffddf37c569
massimilianosentineri <max.sentineri@gmail.com>
2023-06-03 12:53:45 +0200

    adding player animation for movement (frow,back,left,right)

--- Assets/Scripts/Pirates/PlayerMovement.cs
+++ Assets/Scripts/Pirates/PlayerMovement.cs
@@ -8,12 +8,14 @@ public class PlayerMovement : MonoBehaviour
     public Camera camera;    
     public float speed = 1;
     public Rigidbody rb;
-    public Vector3 inputMovement;    
+    public Vector3 inputMovement;
+    public Animator player_an;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        player_an = GetComponent<Animator>();
     }
 
     private void Update()
@@ -32,6 +34,39 @@ public class PlayerMovement : MonoBehaviour
     {
         float playerHorizontalInput = Input.GetAxis("Horizontal");
         float playerVerticalInput = Input.GetAxis("Vertical");
+        if (playerHorizontalInput == 0 && playerVerticalInput == 0) {
+            player_an.SetBool("iswalking", false);
+            player_an.SetBool("iswalking_back", false);
+            player_an.SetBool("iswalking_right", false);
+            player_an.SetBool("iswalking_left", false);
+        }
+        else if (playerHorizontalInput > 0 && playerVerticalInput==0)
+        {
+            player_an.SetBool("iswalking_right", true);
+            player_an.SetBool("iswalking", false);
+            player_an.SetBool("iswalking_back", false);
+            player_an.SetBool("iswalking_left", false);
+        }
+        else if (playerHorizontalInput < 0 && playerVerticalInput == 0)
+        {
+            player_an.SetBool("iswalking_right", false);
+            player_an.SetBool("iswalking", false);
+            player_an.SetBool("iswalking_back", false);
+            player_an.SetBool("iswalking_left", true);
+        }
+        else if (playerVerticalInput < 0)
+        {
+            player_an.SetBool("iswalking_back", true);
+            player_an.SetBool("iswalking", false);
+            player_an.SetBool("iswalking_right", false);
+            player_an.SetBool("iswalking_left", false);
+        }
+        else if (playerVerticalInput > 0) {
+            player_an.SetBool("iswalking", true); 
+            player_an.SetBool("iswalking_back", false);
+            player_an.SetBool("iswalking_right", false);
+            player_an.SetBool("iswalking_left", false);
+        }
         Vector3 forward = camera.transform.forward;
         Vector3 right = camera.transform.right;
         forward.y = 0;
