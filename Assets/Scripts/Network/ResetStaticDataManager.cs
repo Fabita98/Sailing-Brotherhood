294dd572e08f70df8dde702d42a571d38538da2d
Fabrizio <fabrizio.campo@studenti.unimi.it>
2023-06-12 06:14:26 +0200

    ResetStaticDataManager added
    Other changes are stashed for the startMatch()

---
+++ Assets/Scripts/Network/ResetStaticDataManager.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetStaticDataManager : MonoBehaviour
+{
+    private void Awake()
+    {   //check player assignements for 1 ship first
+        //OnBoardBehaviour.ResetStaticData(); 
+        PlayerMovement.ResetStaticData();        
+    }
+
+}
