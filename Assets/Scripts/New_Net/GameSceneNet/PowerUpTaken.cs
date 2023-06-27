8679278bc07304936a448a1a24532299b3fad8a6
Fabrizio <fabrizio.campo@studenti.unimi.it>
2023-06-27 18:24:21 +0200

    PowerUP_UI fixed
    Script simplified and now text appears to everyone

--- Assets/Scripts/New_Net/GameSceneNet/PowerUpTaken.cs
+++ Assets/Scripts/New_Net/GameSceneNet/PowerUpTaken.cs
@@ -8,50 +8,36 @@ public class PowerUpTaken : MonoBehaviour
     [SerializeField]
     private Text speed, smallCannon, barrel, reload;
     public GameObject powerUpUI;
-    public GameObject shipA,shipB;
 
     public static PowerUpTaken Instance { get; private set; }
 
 
     private void Start()
-    {
-        HideUI();   
+    {           
     }   
 
     public void ShowSpeedPowerUpTaken(GameObject s)
-    {       if (s == shipA || s == shipB)
-        {
-            ShowUI();
+    {                 
             speed.text = s.name + " gained a Speed boost!";
-            StartCoroutine(HideAfterDelay(speed));
-        }       
+            StartCoroutine(HideAfterDelay(speed));               
     }
 
     public void ShowSmallcannonPowerUpTaken(GameObject s)
-    {       if (s == shipA || s == shipB)
-        {
-            ShowUI();
+    {        
             smallCannon.text = s.name + " gained a granted Shot!";
-            StartCoroutine(HideAfterDelay(smallCannon));
-        }       
+            StartCoroutine(HideAfterDelay(smallCannon));              
     }
     
     public void ShowBarrelPowerUpTaken(GameObject s)
-    {       if (s == shipA || s == shipB)
-        {
-            ShowUI();
+    {        
             barrel.text = s.name + " gained a Barrel!";
-            StartCoroutine(HideAfterDelay(barrel));
-        }       
+            StartCoroutine(HideAfterDelay(barrel));               
     }
 
     public void ShowReloadPowerUpTaken(GameObject s)
-    {       if (s == shipA || s == shipB)
-        {
-            ShowUI();
+    {        
             reload.text = s.name + " gained a fast Reload!";
-            StartCoroutine(HideAfterDelay(reload));
-        }       
+            StartCoroutine(HideAfterDelay(reload));               
     }
 
     public void ShowUI()
