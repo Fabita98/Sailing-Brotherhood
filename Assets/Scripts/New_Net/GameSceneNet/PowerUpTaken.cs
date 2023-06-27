b83eb47d83ed2cf9786f5abf59fab3338e943202
Fabrizio <fabrizio.campo@studenti.unimi.it>
2023-06-27 20:05:14 +0200

    PowerUpUI working
    Added Show() to renable text on every collected powerup instance

--- Assets/Scripts/New_Net/GameSceneNet/PowerUpTaken.cs
+++ Assets/Scripts/New_Net/GameSceneNet/PowerUpTaken.cs
@@ -10,32 +10,31 @@ public class PowerUpTaken : MonoBehaviour
     public GameObject powerUpUI;
 
     public static PowerUpTaken Instance { get; private set; }
-
-
-    private void Start()
-    {           
-    }   
-
+    
     public void ShowSpeedPowerUpTaken(GameObject s)
-    {                 
+    {
+            Show(speed);
             speed.text = s.name + " gained a Speed boost!";
             StartCoroutine(HideAfterDelay(speed));               
     }
 
     public void ShowSmallcannonPowerUpTaken(GameObject s)
-    {        
+    {                    
+            Show(smallCannon);
             smallCannon.text = s.name + " gained a granted Shot!";
             StartCoroutine(HideAfterDelay(smallCannon));              
     }
     
     public void ShowBarrelPowerUpTaken(GameObject s)
-    {        
+    {                    
+            Show(barrel);
             barrel.text = s.name + " gained a Barrel!";
             StartCoroutine(HideAfterDelay(barrel));               
     }
 
     public void ShowReloadPowerUpTaken(GameObject s)
     {        
+            Show(reload);
             reload.text = s.name + " gained a fast Reload!";
             StartCoroutine(HideAfterDelay(reload));               
     }
@@ -43,6 +42,11 @@ public class PowerUpTaken : MonoBehaviour
     public void ShowUI()
     {
         powerUpUI.SetActive(true);
+    }
+    
+    public void Show(Text t)
+    {
+        t.enabled = true;
     }
 
     public void HideUI()
